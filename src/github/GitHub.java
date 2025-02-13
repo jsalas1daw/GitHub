b5de32cabfe3f5e49be00142731147842548768f
Daw1 <Daw1@MEDREGAL04>
2025-02-13 14:44:36 +0000

    Versión 0 [20250213]: Inicio de Repositorio

---
+++ src/github/GitHub.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package github;
+
+/**
+ *
+ * @author Pablo Senande Sendón <B06H15105V@canariaseducacion.es>
+ */
+public class GitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Inicio de práctica 4.3:GitHub + NetBeans");
+    }
+    
+}
